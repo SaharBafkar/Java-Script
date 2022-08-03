c10cededaa2599fb4a7ce70baa2a7b62053a845d
Sahar Bafkar <104864189+SaharBafkar@users.noreply.github.com>
2022-08-03 12:37:17 +0430

    Add files via upload

---
+++ projects/do-converter-celsius-to-fahrenheit-mini-project/script.js
@@ -0,0 +1,25 @@
+﻿let $ = document
+
+const firstValue = $.querySelector('.C')
+const secondValue = $.querySelector('.F')
+const converter = $.querySelector('#converter')
+const result = $.querySelector('.result')
+const convertButton = $.querySelector('.convertButton')
+const resetButton = $.querySelector('.resetButton')
+const changeButton = $.querySelector('.changeButton')
+
+function convert () {
+    console.log('Convert')
+}
+
+function reset () {
+    console.log('Reset')
+}
+
+function swap () {
+    console.log('Change')
+}
+
+convertButton.addEventListener('click', convert)
+resetButton.addEventListener('click', reset)
+changeButton.addEventListener('click', swap)
